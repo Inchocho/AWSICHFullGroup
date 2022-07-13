a6a5e14f602ceb2d494beac69daea711a98ce818
EZEN-136 <EZEN-136@DESKTOP-42ODUSL>
2022-07-13 15:52:49 +0900

    수업 동기분들 이니셜 프로젝트 파일 입니다.

---
+++ CHS/src/CHS.java
@@ -0,0 +1,30 @@
+
+public class CHS {
+
+	public static void main(String[] args) {
+				
+		String name1 = "LYB";
+		String name2 = "KSJ";
+		String name3 = "PYM";
+		String name4 = "OGH";
+		String name5 = "GMJ";
+		String name6 = "KJS";
+		String name7 = "KTH";
+		String name8 = "NKH";
+		String name9 = "PYM";
+		String name10 = "SDY";
+		String name11 = "ABK";
+		String name12 = "CEJ";
+		String name13 = "LJH";
+		String name14 = "JYS";
+		String name15 = "HWJ";
+				
+		System.out.println(name1+" "+name2+" "+name3);
+		System.out.println(name4+" "+name5+" "+name6);
+		System.out.println(name7+" "+name8+" "+name9);
+		System.out.println(name10+" "+name11+" "+name12);
+		System.out.println(name13+" "+name14+" "+name15);
+
+	}
+
+}
