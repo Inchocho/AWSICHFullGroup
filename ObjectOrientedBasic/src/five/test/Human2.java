793e0d244997cceb59741c8083f17a02309d8125
EZEN-136 <EZEN-136@DESKTOP-42ODUSL>
2022-07-21 15:30:16 +0900

    생성자연습

---
+++ ObjectOrientedBasic/src/five/test/Human2.java
@@ -0,0 +1,15 @@
+package five.test;
+
+public class Human2 {
+	String name = "이동현";
+	int age = 27;
+	String gender = "남";
+	String mbti = "ISTP";
+	
+	void profile() {
+		System.out.println("이름: " + name);
+		System.out.println("나이: " + age);
+		System.out.println("성별: " + gender);
+		System.out.println("MBTI: " + mbti);
+	}
+}
--- ObjectOrientedBasic/src/five/test/print.java
+++ ObjectOrientedBasic/src/five/test/print.java
@@ -3,10 +3,14 @@ package five.test;
 public class print {
 
 	public static void main(String[] args) {
-		
+
 		Human human = new Human("이동현", "남", "ISTP", 27);
-		
+
+		Human2 human2 = new Human2();
+
 		human.profile();
-		
+
+		human2.profile();
+
 	}
 }
