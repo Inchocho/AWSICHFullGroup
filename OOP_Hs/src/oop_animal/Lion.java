efc98e58f53b897539419d2ac5441e69316bdccf
EZEN-136 <EZEN-136@DESKTOP-42ODUSL>
2022-07-25 19:55:55 +0900

    Getter/Setter 기능 추가 (MVC패턴의 시작점)

--- OOP_Hs/src/oop_animal/Lion.java
+++
@@ -1,8 +0,0 @@
-package oop_animal;
-
-public class Lion extends Animal implements Predator{
-
-	public void getFood() {
-		System.out.println("사자고기");
-	}
-}
--- OOP_Hs/src/oop_basic/Counter.java
+++
@@ -1,6 +0,0 @@
-package oop_basic;
-
-public class Counter {
-	
-	 int count = 0;
-}
