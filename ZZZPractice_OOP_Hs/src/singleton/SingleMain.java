2023fe24420bdf4e6254259c47465fd30572dfd2
EZEN-136 <EZEN-136@DESKTOP-42ODUSL>
2022-07-25 20:42:36 +0900

    싱글톤 설명추가

--- ZZZPractice_OOP_Hs/src/singleton/SingleMain.java
+++ ZZZPractice_OOP_Hs/src/singleton/SingleMain.java
@@ -3,8 +3,9 @@ package singleton;
 public class SingleMain {
 
 	public static void main(String[] args) {
-		Singleton sing = Singleton.getInstance();
-		Singleton sing2 = Singleton.getInstance();
-		System.out.println(sing == sing2);
+//		getInstance()메소드를 통해 만든 Singleton클래스타입 인스턴스(객체)가 한개임을 확인하기 위함
+		Singleton sing = Singleton.getInstance();	//new연산자를 통해 Singleton클래스 인스턴스가 생성됨
+		Singleton sing2 = Singleton.getInstance();	//one != null이므로 만들어진 인스턴스 반환
+		System.out.println(sing == sing2);	// true
 	}
 }
--- ZZZPractice_OOP_Hs/src/singleton/Singleton.java
+++ ZZZPractice_OOP_Hs/src/singleton/Singleton.java
@@ -4,14 +4,14 @@ public class Singleton {
 
 	private static Singleton one;	//접근제어자:private, static 제어자(클래스 생성시 한번만), 싱글톤클래스 타입 변수 one
 	
-	public Singleton() {	//생성자(default 생성자)
+	private Singleton() {	//생성자(default 생성자) --> 기본생성자를 private를 접근제어자 선언(즉 생성자로 생성불가하게 막음)
 		
 	}
 	
-	public static Singleton getInstance() {	//static 메소드
-		if(one == null) {
-			one = new Singleton();
+	public static Singleton getInstance() {	//static 메소드 public접근제어자 -> 싱글톤클래스타입의 getInstance 메소드 생성
+		if(one == null) {			//Singleton타입 변수 one이 null이면 
+			one = new Singleton();	//Singleton new연산자를 통해 생성자 호출 --> 즉 한번만 생성자를 만듬
 		}
-		return one;
+		return one;					//null이아니면 one을 반환 즉 getInstance()를 통해 만든 객체는 하나임 
 	}
 }
